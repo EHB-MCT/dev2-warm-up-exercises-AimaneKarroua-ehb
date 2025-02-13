439ea37f07aff91bb3c224304be8defb79f04a16
Aimane Karroua <aimane.karroua@student.ehb.be>
2025-02-13 16:10:31 +0100

    klaar oef 1

--- main.js
+++ main.js
@@ -5,9 +5,28 @@ import {
 } from './lib.js';
 
 runTitle();
+warmup1();
 
 function warmup1() {
     console.log("Exercise week 1");
+
+    const button = document.querySelector('#button-1a')
+    button.addEventListener("click", function() {
+    document.querySelector("#content-1").innerHTML = "<p>Hallo, dit is de oefening van Aimane</p>";
+    });
+
+    const button1b = document.querySelector('#button-1b')
+    button1b.addEventListener("click", function() {
+    document.querySelector("#section-1").style.backgroundColor= "#fff000";
+    });
+
+    const button1c = document.querySelector("#button-1c")
+    button1c.addEventListener("click", function() {
+        document.getElementById("content-1").innerHTML += "<h2>Je bent er klaar voor!</h2>";
+    });
+    
+    
+    
 }
 
 function warmup2() {
